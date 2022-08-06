debeff3dc14ca65034984918e228cd1b441e6c3b
Cynthia Ilojeme <cynthiaezinne1@gmail.com>
2022-08-06 02:09:00 +0100

    add alert styles/more routes

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import Button from "../../components/Button/Button";
+import Input from "../../components/Input/Input";
 
 const SignIn = () => {
   return (
-    <div>SignIn</div>
-  )
-}
+    <div>
+      <Input
+        label="First name"
+        type="text"
+        name="first_name"
+        onChange={() => {}}
+        placeholder="Enter first name"
+      />
+      <Button text="Submit" onClick={() => {}} marginTop={30}/>
+    </div>
+  );
+};
 
-export default SignIn
+export default SignIn;
